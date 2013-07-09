770f637bb5ebeda53839e54448b186427ed69e65
meet2me <meet2me@github.com>
2013-07-09 14:41:04 +0530

    Tweets saved to DB

--- dbHandle.js
+++ dbHandle.js
@@ -4,7 +4,8 @@ var Server = require('mongodb').Server;
 var BSON = require('mongodb').BSON;
 var ObjectID = require('mongodb').ObjectID;
 
-
+var tweet='';
+var created_at='';
 
 DBHandler = function(host, port) {
   this.db= new Db('tweetDb', new Server(host, port, {safe: false}, {auto_reconnect: true}, {}));
@@ -17,7 +18,6 @@ DBHandler = function(host, port) {
   	});
 };
 
-
 DBHandler.prototype.getCollection = function(callback) {
 	// body...
 	this.db.collection('tweets',function(error, tweet_collection) {
@@ -28,19 +28,23 @@ DBHandler.prototype.getCollection = function(callback) {
 
 DBHandler.prototype.saveTweets = function(tweets,callback){
 	this.getCollection(function(error,tweet_collection) {
-      if( error ) callback(error);
-      else {
-                
-
-        for(var i =0;i< tweets.length;i++ ) {
-          var tweet = tweets.tweest[i].text;
-          console.log("DB Tweet: ",tweet);
-        tweet_collection.insert(tweet,function() {
-          callback(null, tweets);
+    var clubTweet=[];
+    var i=tweets.length;
+    if( error ) {
+      callback(error);
+      }
+    else {
+      for(var i =0;i< tweets.length;i++ ) {
+        tweet = tweets[i].text;
+        created_at = tweets[i].created_at;
+        //clubTweet.push(tweet);
+        console.log("DB Tweet: ",tweet);
+        tweet_collection.insert({text :tweet, time: created_at},function(){
+          callback(null,tweet);
         });
-        }
-
-      }   
-	});
+      }
+      //console.log("Tweet Out: ",clubTweet);
+    }
+  });
 };
 exports.DBHandler = DBHandler;

2426c897dcf3bd28ee958b9c13247c8427dea6c7
meet2me <pmeet95@gmail.com>
2013-07-11 13:22:08 +0530

    after displaying home-page

--- app.js
+++ app.js
@@ -2,13 +2,11 @@ var http = require('http');
 var path = require('path');
 var https = require('https');
 var fs = require('fs');
-
 var express = require('express');
 
 var passport = require('passport');
 var app = express();
 require('./configureApp')(app);
-//require('./passport')(passport);
 var TwitterStrategy = require('passport-twitter').Strategy;
 
 var obj = JSON.parse(fs.readFileSync('consumerCredentials.json', 'utf8'));
@@ -18,6 +16,7 @@ var callbackURL = obj.callback;
 
 var DBHandler = require('./dbHandle.js').DBHandler;
 var dbHandler= new DBHandler('localhost', 27017);
+var data ='';
 
 passport.use(new TwitterStrategy({
 	consumerKey: consumerKey,
@@ -29,21 +28,22 @@ passport.use(new TwitterStrategy({
       delete profile._raw;
       profile.access_token = token;
       profile.token_secret = tokenSecret;
-
-      //console.log("Test");
+      //data = JSON.parse(profile);
+      data=profile;
+      console.log("Data :",data);
+      //console.log("Profile: ",profile);
       dbHandler.findOrCreateUser(profile, function(error, user){
-          if(error){
-            console.log("DB Error : ", error);
-          }
-          return done(null, user);
-        });
+        if(error){
+          console.log("DB Error : ", error);
+        }
+        return done(null, user);
+      });
   	}
 ));
 
 passport.serializeUser(function(user, done) {
   done(null, user.id);
 });
-
 passport.deserializeUser(function(id, done) {
   //User.findById(id, function(err, user) {
   dbHandler.findUser(id, done);
@@ -54,15 +54,13 @@ app.get('/',function(req, res) {
  });
 
 app.get('/auth/twitter', passport.authenticate('twitter'));
-
-app.get('/callback', 
+app.get('/auth/twitter/callback', 
   passport.authenticate('twitter', { successRedirect: '/showTimeline',
                                      failureRedirect: '/login' }));
-/*
-app.get('/login',function(req, res) {
-    res.send('<html><body><a href="/auth/twitter">Sign in with Twitter</a></body></html>');
- });
-*/
+
+app.get('/showTimeline',function(req,res){
+  res.render('home.jade',{data : data});
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
